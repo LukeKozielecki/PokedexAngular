baaa3eb725acc40ada1e5ec683d2b37c66a894cf
Luke <lucjan.kozielecki@gmail.com>
2025-08-10 18:40:36 +0200

    fix: evolution-chain-species would not update on transition to similar Pokémon
    - replaced ngOnInit with ngOnChanges block
    - ngOnChanges observes changes to 'species' input property

--- src/app/features/pokemon/presentation/pages/pokemon-details/components/evolution-chain-species/evolution-chain-species.component.ts
+++ src/app/features/pokemon/presentation/pages/pokemon-details/components/evolution-chain-species/evolution-chain-species.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {EvolutionSpecies} from '../../../../../domain/model/EvolutionChain';
 import {map, Observable} from 'rxjs';
 import {Pokemon} from '../../../../../domain/model/Pokemon';
@@ -7,15 +7,14 @@ import {CommonModule} from '@angular/common';
 import {Router} from '@angular/router';
 import {NAVIGATION_DELAY} from '../../../../../../../shared/constants/app.constants';
 import {PokemonDetails} from '../../../../../domain/model/PokemonDetails';
-import {PokemonDetailsHeader} from '../details-header/details-header.component';
 
 @Component({
   selector: 'app-evolution-chain-species',
-  imports: [CommonModule, PokemonDetailsHeader],
+  imports: [CommonModule],
   templateUrl: './evolution-chain-species.html',
   styleUrl: './evolution-chain-species.scss'
 })
-export class EvolutionChainSpeciesComponent implements OnInit {
+export class EvolutionChainSpeciesComponent implements OnChanges {
   @Input({required: true}) species!: EvolutionSpecies;
   @Input() pokemonDetails!: PokemonDetails
   pokemon$!: Observable<Pokemon | undefined>;
@@ -25,10 +24,12 @@ export class EvolutionChainSpeciesComponent implements OnInit {
     private router: Router
   ) {}
 
-  ngOnInit(): void {
-    this.pokemon$ = this.dataService.getAllPokemon().pipe(
-      map(allPokemon => allPokemon.find(pokemon => pokemon.name === this.species.name))
-    );
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['species']) {
+      this.pokemon$ = this.dataService.getAllPokemon().pipe(
+        map(allPokemon => allPokemon.find(pokemon => pokemon.name === this.species.name))
+      );
+    }
   }
 
   onPokemonVariantClick(id: number | string) {
