db4c3ef6070a844efc331ceb1b5cf314547dba97
Luke <lucjan.kozielecki@gmail.com>
2025-08-12 15:53:35 +0200

    fix: add databaseURL to `firebaseConfig.ts`
    Rationale: This link was not included by default in firebaseConfig.ts provided by firebase. Needed to add databaseUrl manually from console error.

--- src/app/firebaseConfig.ts
+++ src/app/firebaseConfig.ts
@@ -6,7 +6,8 @@ const firebaseConfig = {
   projectId: "pokdex-v2",
   storageBucket: "pokdex-v2.firebasestorage.app",
   messagingSenderId: "371144231830",
-  appId: "1:371144231830:web:659f5ae6e48490e2f1ebb7"
+  appId: "1:371144231830:web:659f5ae6e48490e2f1ebb7",
+  databaseURL: "https://pokdex-v2-default-rtdb.europe-west1.firebasedatabase.app"
 };
 
 export const app = initializeApp(firebaseConfig);
